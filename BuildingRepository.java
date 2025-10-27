1d4f3569d0ab96f471cae6d84f416530835c4f13
Phan Duy Khương <handuykhuong05012005@gmail.com>
2025-10-27 14:10:19 +0700

    Delete BuildingRepository.java
    delete eclipse config files

--- BuildingRepository.java
+++
@@ -1,8 +0,0 @@
-package com.javaweb.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.javaweb.entity.BuildingEntity;
-
-public interface BuildingRepository extends JpaRepository<BuildingEntity, Long> {
-    // Truy vấn tài sản
-}
