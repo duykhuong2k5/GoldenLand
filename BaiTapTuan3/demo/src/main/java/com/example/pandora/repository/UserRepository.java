a4b729b4121ede312324a153cb72a7c639a212d0
duykhuong2k5 <handuykhuong05012005@gmail.com>
2026-01-22 14:45:43 +0700

    Remove BaiTapTuan3 folder

--- BaiTapTuan3/demo/src/main/java/com/example/pandora/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.pandora.repository;
-
-import com.example.pandora.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
-}
--- BaiTapTuan3/demo/src/main/java/com/example/pandora/service/EmailService.java
+++
@@ -1,7 +0,0 @@
-package com.example.pandora.service;
-
-import jakarta.mail.MessagingException;
-
-public interface EmailService {
-    void sendOtp(String to, String otp) throws MessagingException;
-}
